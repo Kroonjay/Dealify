bb8bb5b84e3d7281d526aacd54ba72a333d3903c
Scout <scoutfullner96@gmail.com>
2021-03-25 12:54:38 -0700

    Fixed bug with SetOverdueCraigslistQueries sproc not properly updating rows

--- database/sprocs/set_overdue_craigslist_tasks.sql
+++ database/sprocs/set_overdue_craigslist_tasks.sql
@@ -1,6 +1,6 @@
 DELIMITER //
 CREATE PROCEDURE SetOverdueCraigslistQueries()
 BEGIN
-UPDATE CraigslistQueries SET query_status = 2 WHERE ADDDATE(last_execution_at, INTERVAL @interval_mins MINUTE) < CURRENT_TIMESTAMP AND query_status = 0;
+UPDATE CraigslistQueries SET query_status = 2 WHERE ADDDATE(last_execution_at, INTERVAL interval_mins MINUTE) < CURRENT_TIMESTAMP AND query_status = 0;
 SELECT COUNT(query_id) FROM CraigslistQueries WHERE query_status = 2;
 END //
